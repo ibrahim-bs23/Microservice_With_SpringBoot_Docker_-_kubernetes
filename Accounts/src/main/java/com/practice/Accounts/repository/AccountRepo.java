eba064e95baa4130dca2ef54686f759783f263d6
Ibrahim Adnan <ibrahim.adnan@brainstation-23>
2024-10-18 18:58:20 +0600

    Made microservice resilient

--- Accounts/src/main/java/com/practice/Accounts/repository/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.practice.Accounts.repository;
-
-import com.practice.Accounts.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepo extends JpaRepository<Account, Long> {
-}
--- Accounts/src/main/java/com/practice/Accounts/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.practice.Accounts.repository;
-
-import com.practice.Accounts.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Long> {
-}
